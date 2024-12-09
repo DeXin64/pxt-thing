661882169ff28d0c50ff29a63118455db34cd974
DeXin64 <129580034+DeXin64@users.noreply.github.com>
2024-12-09 19:28:22 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,4 +4,28 @@ namespace thing{
   export function test():number{
     return 1;
   }
+  export class FwdEduDial {
+    private _dialClient: jacdac.RotaryEncoderClient
+    private _cwAction: (delta: number) => void
+    private _ccwAction: (delta: number) => void
+
+    constructor(role: string) {
+      this._dialClient = new jacdac.RotaryEncoderClient(role)
+    }
+    
+    //% group="Dial"
+    //% block="clicks per full turn"
+    //% blockId=fwd_dial_get_clicks_per_turn
+    //% color="#00cc00"
+    clicksPerTurn(): number { return this._dialClient.clicksPerTurn() }
+    
+    //% group="Dial"
+    //% block="%dial position"
+    //% blockId=fwd_dial_get_position
+    //% color="#00cc00"
+     position(): number { return this._dialClient.position() }
+    
+    
+
+  }
 }
